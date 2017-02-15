b6be7804272bcbaf4c2f6720f32cc930fd4e0c46
Trent Aaron Ohrwalder <trent.ohh@gmail.com>
2017-02-15 15:58:55 +1100

    Setup of OpenGL with creating the window and able to exit when command is pressed while creating the main gameLoop which will function the game logic, update and render code.

--- OpenGL - Engine/main.cpp
+++ OpenGL - Engine/main.cpp
@@ -1,8 +1,43 @@
+#include <gl_core_4_4.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-
-
-
-void main()
+int main()
 {
+
+	if (glfwInit() == false)
+		return -1;
+
+	// Code here
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL Game Engine", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -2;
+	}
+
+	// Rest of code goes here.
+
+	int major = ogl_GetMajorVersion();
+	int minor = ogl_GetMinorVersion();
+
+	std::cout << "GL Version is " << major << "." << minor << '\n';
+
+	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+	{
+		// Game logic and update code goes here.
+		// So does render code.
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 
 }
